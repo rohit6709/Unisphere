51a9b1c5563350d5c9d933606416fed683778741
rohit6709 <dograrohit954@gmail.com>
2026-03-19 00:47:06 +0530

    Removed old controllers and routes

--- backend/src/routers/csv.routes.js
+++
@@ -1,8 +0,0 @@
-import Router from 'express';
-import { uploadStudents } from '../controllers/csv.controller.js';
-import { upload } from '../middlewares/upload.middleware.js';
-
-const router = Router();
-router.route('/upload-students').post(upload.single('file'), uploadStudents);
-
-export default router;
