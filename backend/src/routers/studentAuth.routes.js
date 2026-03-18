67b9e131661109ef73b25a567625521dbda5916f
rohit6709 <dograrohit954@gmail.com>
2026-03-19 00:37:58 +0530

    Added student auth routes

---
+++ backend/src/routers/studentAuth.routes.js
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+import { loginUser, logoutUser, changeCurrentPassword, forgotPassword, resetPassword, getProfile, refreshAccessToken } from '../controllers/studentAuth.controller.js';
+import { verifyJWT } from '../middlewares/auth.middleware.js';
+import { uploadStudents } from '../controllers/studentCSV.controller.js';
+import { upload } from '../middlewares/upload.middleware.js';
+import { verifyRole } from '../middlewares/role.middleware.js';
+
+const router = Router();
+
+router.route('/login').post(loginUser);
+router.route('/refresh-token').post(refreshAccessToken);
+router.route('/forgot-password').post(forgotPassword);
+router.route('/reset-password/:token').post(resetPassword);
+
+router.route('/logout').post(verifyJWT, logoutUser);
+router.route('/change-password').post(verifyJWT, changeCurrentPassword);
+router.route('/profile').get(verifyJWT, getProfile);
+
+router.route('/upload-csv').post(verifyJWT, verifyRole("admin","superadmin"), upload.single('file'), uploadStudents);
+
+
+export default router;
+
+//check api works or not!
+
