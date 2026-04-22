f8693c94e1ca5e8fde55ce3a3dc4b951f1184077
rohit6709 <dograrohit954@gmail.com>
2026-04-22 23:14:51 +0530

    Updated notification controller

--- backend/src/controllers/notification.controller.js
+++ backend/src/controllers/notification.controller.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import { Notification } from '../models/Notification.model.js';
+import { Notification } from '../models/notification.model.js';
 import { ApiError } from '../utils/ApiError.js';
 import { ApiResponse } from '../utils/ApiResponse.js';
 import { asyncHandler } from '../utils/asyncHandler.js';
