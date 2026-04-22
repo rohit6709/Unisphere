b9db383638918a88bceb1e4cfa08a7699a069c92
rohit6709 <dograrohit954@gmail.com>
2026-04-22 23:16:57 +0530

    Updated student model

--- backend/src/models/student.model.js
+++ backend/src/models/student.model.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import { INTEREST_CATEGORIES } from '../constants/interests';
+import { INTEREST_CATEGORIES } from '../constants/interests.js';
 
 const studentSchema = new mongoose.Schema({
    name: {
