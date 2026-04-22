552601fbbe31da6f9d35cee77783937ba88cf9fc
rohit6709 <dograrohit954@gmail.com>
2026-04-22 23:32:13 +0530

    Updated notification service

--- backend/src/services/notificationService.js
+++ backend/src/services/notificationService.js
@@ -1,4 +1,4 @@
-import { Notification } from "../models/Notification.model/js";
+import { Notification } from "../models/notification.model.js";
 
 const PRIORITY_LABEL = { low: "LOW", normal: "NORMAL", high: "HIGH", critical: "CRITICAL" };
 
