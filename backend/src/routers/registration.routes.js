44a69c7dd4306d2cc9398e30a3780f1892577f28
rohit6709 <dograrohit954@gmail.com>
2026-04-22 23:30:47 +0530

    Updated registration routes

--- backend/src/routers/registration.routes.js
+++ backend/src/routers/registration.routes.js
@@ -1,15 +1,17 @@
 import { Router } from "express";
-import { registerForEvent, unregisterFromEvent, getEventRegistrations, getMyRegistrations } from "../controllers/registration.controller.js";
+import { registerForEvent, unregisterFromEvent, getEventRegistrations, getMyRegistrations, getAllRegistrations, exportRegistrations } from "../controllers/registration.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { verifyRole } from "../middlewares/role.middleware.js"; 
 
 const registrationRouter = Router();
 
 registrationRouter.route("/my-registrations").get(verifyJWT, verifyRole("student"), getMyRegistrations);
+registrationRouter.route("/all").get(verifyJWT, verifyRole("admin", "superadmin"), getAllRegistrations);
 
 registrationRouter.route("/:eventId/register").post(verifyJWT, verifyRole("student"), registerForEvent);
 registrationRouter.route("/:eventId/unregister").delete(verifyJWT, verifyRole("student"), unregisterFromEvent);
 
 registrationRouter.route("/:eventId/registrations").get(verifyJWT, verifyRole("admin", "superadmin", "faculty", "hod"), getEventRegistrations);
+registrationRouter.route("/:eventId/export").get(verifyJWT, verifyRole("admin", "superadmin", "faculty", "hod"), exportRegistrations);
 
 export default registrationRouter;
