b03a43c327d16eedf3a6c0d385b9b5511b556a24
rohit6709 <dograrohit954@gmail.com>
2026-04-23 23:54:06 +0530

    Updated chat routes

--- backend/src/routers/chat.routes.js
+++ backend/src/routers/chat.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { getMessageHistory, deleteMessage } from "../controllers/chat.controller.js";
+import { getMessageHistory, deleteMessage, getMyRooms, searchChatUsers, createDirectConversation } from "../controllers/chat.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const chatRouter = Router();
 
+chatRouter.route("/rooms").get(verifyJWT, getMyRooms);
+chatRouter.route("/users/search").get(verifyJWT, searchChatUsers);
+chatRouter.route("/direct-conversations").post(verifyJWT, createDirectConversation);
 chatRouter.route("/:roomType/:roomId/messages").get(verifyJWT, getMessageHistory);
 chatRouter.route("/:roomType/:roomId/messages/:messageId").delete(verifyJWT, deleteMessage);
 
