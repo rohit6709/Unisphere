367187dc47abf2b226fd373e83fff2c211d121f6
rohit6709 <dograrohit954@gmail.com>
2026-04-23 23:55:32 +0530

    Updated registration route

--- backend/src/routers/registration.routes.js
+++ backend/src/routers/registration.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerForEvent, unregisterFromEvent, getEventRegistrations, getMyRegistrations, getAllRegistrations, exportRegistrations } from "../controllers/registration.controller.js";
+import { registerForEvent, unregisterFromEvent, getEventRegistrations, getMyRegistrations, getAllRegistrations, exportRegistrations, updateRegistrationStatus, bulkUpdateRegistrationStatus } from "../controllers/registration.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { verifyRole } from "../middlewares/role.middleware.js"; 
 
@@ -12,6 +12,8 @@ registrationRouter.route("/:eventId/register").post(verifyJWT, verifyRole("stude
 registrationRouter.route("/:eventId/unregister").delete(verifyJWT, verifyRole("student"), unregisterFromEvent);
 
 registrationRouter.route("/:eventId/registrations").get(verifyJWT, verifyRole("admin", "superadmin", "faculty", "hod"), getEventRegistrations);
+registrationRouter.route("/:eventId/registrations/bulk-status").patch(verifyJWT, verifyRole("admin", "superadmin"), bulkUpdateRegistrationStatus);
+registrationRouter.route("/:eventId/registrations/:registrationId/status").patch(verifyJWT, verifyRole("admin", "superadmin"), updateRegistrationStatus);
 registrationRouter.route("/:eventId/export").get(verifyJWT, verifyRole("admin", "superadmin", "faculty", "hod"), exportRegistrations);
 
 export default registrationRouter;
