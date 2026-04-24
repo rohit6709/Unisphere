4b4d2e61cfad12ef81f90487dc564f027ae50a29
rohit6709 <dograrohit954@gmail.com>
2026-04-24 11:53:45 +0530

    Added frontend components

---
+++ frontend/src/components/icons/index.js
@@ -0,0 +1,15 @@
+export { ArrowRightIcon } from './ArrowRightIcon';
+export { BellIcon } from './BellIcon';
+export { ChatBubbleIcon } from './ChatBubbleIcon';
+export { CheckCircleIcon } from './CheckCircleIcon';
+export { CirclePatternIcon } from './CirclePatternIcon';
+export { GridIcon } from './GridIcon';
+export { LogoMarkIcon } from './LogoMarkIcon';
+export { MoonIcon } from './MoonIcon';
+export { ShieldCheckIcon } from './ShieldCheckIcon';
+export { SparkIcon } from './SparkIcon';
+export { StudentCapIcon } from './StudentCapIcon';
+export { SunIcon } from './SunIcon';
+export { TeamIcon } from './TeamIcon';
+export { XCircleIcon } from './XCircleIcon';
+
