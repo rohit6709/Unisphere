5cadf1e124dbeece94d167ae1a951624dd5fa90e
rohit6709 <dograrohit954@gmail.com>
2026-04-25 17:49:31 +0530

    Updated dashboard service

--- frontend/src/services/dashboardService.js
+++ frontend/src/services/dashboardService.js
@@ -1,15 +1,13 @@
 import { apiRequest, apiRequestRaw } from './serviceUtils';
-import { getAllClubs } from './clubService';
-import { getAllEvents } from './eventService';
+import { getAllClubs, getMyAdvisedClubs } from './clubService';
+import { getAdviseePendingEvents, getAllEvents, getPublicEvents } from './eventService';
 
 export const getStudentDashboard = (params) => apiRequest('get', '/students/dashboard', { params });
-
-const isNotFoundError = (error) => error?.response?.status === 404;
 
 const getFallbackDashboardData = async () => {
 	const [clubsData, eventsData] = await Promise.all([
 		getAllClubs(),
-		getAllEvents(),
+		getPublicEvents(),
 	]);
 
 	return {
@@ -18,21 +16,37 @@ const getFallbackDashboardData = async () => {
 	};
 };
 
-const getRoleDashboard = async (path) => {
+export const getFacultyDashboard = async () => {
 	try {
-		return await apiRequest('get', path);
-	} catch (error) {
-		if (!isNotFoundError(error)) {
-			throw error;
-		}
+		const [clubsData, eventsData] = await Promise.all([
+			getMyAdvisedClubs(),
+			getAdviseePendingEvents(),
+		]);
 
+		return {
+			clubs: clubsData?.clubs ?? clubsData ?? [],
+			events: eventsData?.events ?? eventsData ?? [],
+		};
+	} catch {
 		return getFallbackDashboardData();
 	}
 };
 
-export const getFacultyDashboard = () => getRoleDashboard('/faculty/dashboard');
+export const getAdminDashboard = async () => {
+	try {
+		const [clubsData, eventsData] = await Promise.all([
+			getAllClubs(),
+			getAllEvents(),
+		]);
 
-export const getAdminDashboard = () => getRoleDashboard('/admin/dashboard');
+		return {
+			clubs: clubsData?.clubs ?? clubsData ?? [],
+			events: eventsData?.events ?? eventsData ?? [],
+		};
+	} catch {
+		return getFallbackDashboardData();
+	}
+};
 
 export const dashboardService = {
 	getStudentDashboard,
