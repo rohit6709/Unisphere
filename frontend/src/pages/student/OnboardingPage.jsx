5d0691ad96deaa139c07afe5ed36cf9112ff972d
rohit6709 <dograrohit954@gmail.com>
2026-04-25 17:44:19 +0530

    Updated onboarding page

--- frontend/src/pages/student/OnboardingPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import StudentOnboardingPage from '@/pages/auth/StudentOnboardingPage';
-
-export default function OnboardingPage() {
-	return <StudentOnboardingPage />;
-}
-
